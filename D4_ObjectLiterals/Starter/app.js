45d3310c4b3ba32eaf9a2c3f1a2e5d9369a45c77
yonatanmk <yonatanmk@gmail.com>
2017-07-27 19:27:03 -0400

    finished D4

--- D4_ObjectLiterals/Starter/app.js
+++ D4_ObjectLiterals/Starter/app.js
@@ -1,1 +1,12 @@
-// Your Javascript Code Goes Here
+var person = {
+	firstname: 'John',
+	lastname: 'Doe',
+	greet: function() {
+		console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+	}
+};
+
+person.greet();
+
+console.log(person['firstname']);
+console.log(person.firstname);
