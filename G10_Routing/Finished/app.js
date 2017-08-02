bafce58b10bd51e84f187f8dfff9fe62b9f8ffb7
yonatanmk <yonatanmk@gmail.com>
2017-08-02 18:59:18 -0400

    G edited

--- G10_Routing/Finished/app.js
+++ G10_Routing/Finished/app.js
@@ -2,11 +2,12 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-    
+
     if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.createReadStream(__dirname + '/index.htm').pipe(res);
     }
-    
+
     else if (req.url === '/api') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         var obj = {
@@ -19,5 +20,5 @@ http.createServer(function(req, res) {
         res.writeHead(404);
         res.end();
     }
-    
+
 }).listen(1337, '127.0.0.1');
