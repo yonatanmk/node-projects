d6c5a33d66abf826fe46eb2857d697f98933bdcd
yonatanmk <yonatanmk@gmail.com>
2017-08-27 13:04:21 -0400

    created blogPost associations

--- MongoWorkSpace/users/src/user.js
+++ MongoWorkSpace/users/src/user.js
@@ -13,6 +13,10 @@ const UserSchema = new Schema({
   },
   likes: Number,
   posts: [PostSchema],
+  blogPosts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'blogPost'
+  }]
 });
 
 UserSchema.virtual('postCount').get(function() {
