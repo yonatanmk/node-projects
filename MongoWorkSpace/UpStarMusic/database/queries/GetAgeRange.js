52930edca0a8a54af12319e54bb1d6e35c7c2de7
yonatanmk <yonatanmk@gmail.com>
2017-09-02 15:33:11 -0400

    min/max queries

--- MongoWorkSpace/UpStarMusic/database/queries/GetAgeRange.js
+++ MongoWorkSpace/UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,21 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minQuery = Artist
+    .find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  const maxQuery = Artist
+    .find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  return Promise.all([minQuery, maxQuery])
+    .then(results => {
+      console.log(results);
+      return { min: results[0], max: results[1] };
+    });
 };
