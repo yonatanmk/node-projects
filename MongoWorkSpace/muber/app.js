fb2269b2820b075bceae67531a116ad5715050ec
yonatanmk <yonatanmk@gmail.com>
2017-09-03 13:05:39 -0400

    working on create driver test

--- MongoWorkSpace/muber/app.js
+++ MongoWorkSpace/muber/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const routes = require('./routes/routes')
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
 const app = express();
 
-// Watch for GET to http://localhost:3030/api
-routes(app)
+app.use(bodyParser.json());
+routes(app);
 
 module.exports = app;
--- MongoWorkSpace/muber/controllers/drivers_controller.js
+++ MongoWorkSpace/muber/controllers/drivers_controller.js
@@ -1,6 +1,15 @@
+const Driver = require('../../models/Driver');
+
 module.exports = {
-  greeting(req, res) {
+  greeting: (req, res) => { // method 1 for storing func in controller
     res.send({ hi: 'there' })
   },
+  create(req, res) { // method 2 for storing func in controller
+    console.log(req.body)
+    res.send({ hi: 'there' })
+  }
 
+  // next: function (req, res) { // method 3 for storing func in controller
+  //
+  // }
 }
