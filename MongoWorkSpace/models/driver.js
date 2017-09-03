0e552b27ec09e96689cbfe9c1be697f0908935f1
yonatanmk <yonatanmk@gmail.com>
2017-09-03 11:58:49 -0400

    created driver model

---
+++ MongoWorkSpace/models/driver.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// const PointSchema = new Schema({
+//   coordinates: { type: [Number], index: '2dsphere' },
+//   type: { type: String, default: 'Point' }
+// });
+
+const DriverSchema = new Schema({
+  email: {
+    type: String,
+    required: true // validation: email is required
+  },
+  driving: {
+    type: Boolean,
+    default: false // adds default value
+  },
+  // geometry: PointSchema
+});
+
+const Driver = mongoose.model('driver', DriverSchema);
+
+module.exports = Driver;
