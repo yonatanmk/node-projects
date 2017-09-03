67453b030a23dfdc05a9e875fc1cfae57fadf313
yonatanmk <yonatanmk@gmail.com>
2017-09-03 13:11:36 -0400

    create driver test successful

--- MongoWorkSpace/muber/app.js
+++ MongoWorkSpace/muber/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise; // fixes a mongoose deprecation error
+mongoose.connect('mongodb://localhost/muber'); // connects to local mongo database
 
 app.use(bodyParser.json());
 routes(app);
--- MongoWorkSpace/muber/controllers/drivers_controller.js
+++ MongoWorkSpace/muber/controllers/drivers_controller.js
@@ -1,12 +1,13 @@
-const Driver = require('../../models/Driver');
+const Driver = require('../models/Driver');
 
 module.exports = {
   greeting: (req, res) => { // method 1 for storing func in controller
     res.send({ hi: 'there' })
   },
   create(req, res) { // method 2 for storing func in controller
-    console.log(req.body)
-    res.send({ hi: 'there' })
+    const DriverProps = req.body;
+    Driver.create(DriverProps)
+      .then(driver => res.send(driver));
   }
 
   // next: function (req, res) { // method 3 for storing func in controller
